299a86aa057ef6b2829ef8faec4d39a00b80c534
= <=>
2023-07-23 03:26:09 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import BoardList from "./routes/BoardList";
+import {Route, Routers} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener nore ferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/board" element={<BoardList/>}/> />
+      </Routes>
   );
 }
 
